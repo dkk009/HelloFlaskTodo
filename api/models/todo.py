b5ee1fe8c053eb901d0fd84e26d5c4ddcebb4b96
Deepak KK <deepak.kk@ymedialabs.com>
2022-12-05 11:32:50 +0530

    Implemented to do creation.

--- api/models/todo.py
+++ api/models/todo.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from enum import Enum
 
 class ToDoStatus(Enum):
-    NOT_YET="not_yet"
+    CREATED="created"
     STARTED="started"
     COMPLETED="completed"
     EXPIRED="expired"
@@ -16,12 +16,23 @@ class Todo(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.Integer(), nullable=False)
     description= db.Column(db.Text())
-    completion_date=db.Column(db.DateTime())
+    completion_date=db.Column(db.DateTime(),default=datetime.utcnow)
     start_date=db.Column(db.DateTime(), default=datetime.utcnow)
     created_date = db.Column(db.DateTime(), default=datetime.utcnow)
-    status = db.Column(db.Enum(ToDoStatus), default=ToDoStatus.NOT_YET)
+    status = db.Column(db.Enum(ToDoStatus), default=ToDoStatus.CREATED)
     category_id=db.Column(db.Integer(), db.ForeignKey('category.id'),nullable=False)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'),nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
 
+    def __init__(self, name, description, start_date, status,category_id,user_id):
+        self.name = name
+        self.description = description
+        self.start_date = start_date
+        self.status = status
+        self.category_id = category_id
+        self.user_id = user_id
+        
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
     def __str__(self) -> str:
         return f"Todo('{self.id}', '{self.name}')"
--- api/todo/views.py
+++ api/todo/views.py
@@ -1,27 +1,71 @@
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restx import Resource,Namespace,fields
-
+from ..models.users import User
+from ..models.resp_data import RespData
+from ..models.todo import Todo
+from flask import request
+from http import HTTPStatus
+from datetime import datetime
+from ..models.todo import ToDoStatus
 todo_name_space = Namespace(name="todo", description='Todo Name space')
-todo_model = todo_name_space.model(
+todo_req_model = todo_name_space.model(
+    'Todo',{
+        'name':fields.String(required=True, description="Todo Name"),
+        'description':fields.String(required=False, description="Todo Description"),
+        'start_date':fields.Integer(required=False, description="Todo start date"),
+        'status':fields.String(required=True, description='Todo Status', enum=["created","started","completed","expired","unknown"]),
+        'category_id':fields.Integer(required=True, description="Category Id"),
+        
+    }
+)
+todo_resp_data = todo_name_space.model(
     'Todo',{
         'name':fields.String(required=True, description="Todo Name"),
         'description':fields.String(required=False, description="Todo Description"),
         'start_date':fields.DateTime(required=False, description="Todo start date"),
-        'created_date':fields.DateTime(required=False, description="Todo created_date"),
-        'status':fields.String(required=True, descriptio='Todo Status', enum=["not_yet","started","completed","expired","unknown"]),
+        'status':fields.String(required=True, description='Todo Status', enum=["created","started","completed","expired","unknown"]),
         'category_id':fields.Integer(required=True, description="Category Id"),
         
+    }
+)
+todo_resp_model = todo_name_space.model(
+    'TodoResp',{
+        'status':fields.Boolean(required=True, description='Status'),
+        'message':fields.String(required=True, description='Response status message'),
+        'data':fields.Nested(todo_resp_data)
     }
 )
 
 @todo_name_space.route('/')
 class Tdodo(Resource):
-    @jwt_required(refresh=True)
+    @jwt_required()
     def get(self):
         return {"message":"Hello Tdoo"}
+   
+    @todo_name_space.expect(todo_req_model)
+    @todo_name_space.marshal_with(todo_resp_model)
+    @jwt_required()
     def post(self):
+        user_name = get_jwt_identity()
+        current_user = User.query.filter_by(user_name = user_name).first()
+        print("I am in todo creation")
+        if(current_user is not None):
+            data = request.get_json()
+            name = data.get("name")
+            description = data.get('description')
+            start_date = datetime.fromtimestamp(data.get('start_date')/1000)
+        
+            status = ToDoStatus(data.get('status'))
+            category_id = data.get('category_id')
+            print("Current User Id:", current_user.id)
+            todo = Todo(name=name,description=description,start_date=start_date,status=status,category_id=category_id,user_id=current_user.id)
+            todo.save()
+            resp = RespData(status=True,message ="Successfully added", data=todo)
+            return resp, HTTPStatus.CREATED
+        else:
+            resp = RespData(status=False, message="Could not found the user")
+            return resp, HTTPStatus.BAD_REQUEST
 
-        pass
 
 @todo_name_space.route('/<int:todo_id>')
 class GetOrUpdateTodo(Resource):
